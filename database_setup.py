5478646467a94e4422b42cdf5ea12019cfc3ef49
alexandrabaturina <alexandra.v.baturina@gmail.com>
2020-03-26 16:43:08 -0700

    Add classes

--- database_setup.py
+++ database_setup.py
@@ -6,5 +6,22 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(50), nullable = False)
+
+
+class CatalogItem(Base):
+    __tablename__ = 'catalog_item'
+
+    name = Column(String(50), nullable = False)
+    id = Column(Integer, primary_key = True)
+    description = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+    
 engine = create_engine('sqlite:///catalogitems.db')
 Base.metadata.create_all(engine)
