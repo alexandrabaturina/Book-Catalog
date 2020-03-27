9a1ae1253aaed2d34ff0d640e4fae9937ce7385c
alexandrabaturina <alexandra.v.baturina@gmail.com>
2020-03-26 17:37:14 -0700

    Add configuration code

--- application.py
+++ application.py
@@ -1,4 +1,14 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Category, CatalogItem
+
+engine = create_engine('sqlite:///catalogitems.db',
+    connect_args={'check_same_thread': False}, echo=True)
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 app = Flask(__name__)
 
