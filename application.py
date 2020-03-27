a6e3877d7cf02cafb7086dfb0a91062bf6038423
alexandrabaturina <alexandra.v.baturina@gmail.com>
2020-03-26 17:55:45 -0700

    Add newCatalogItem function

--- application.py
+++ application.py
@@ -17,6 +17,21 @@ def showCategories():
     categories = session.query(Category).all()
     return render_template('categories.html', categories=categories)
 
+# Create a new category item
+@app.route(
+    '/category/<int:category_id>/list/new/', methods=['GET', 'POST'])
+def newCatalogItem(category_id):
+        if request.method == 'POST':
+        newItem = CatalogItem(name=request.form['name'],
+                            description=request.form['description'],
+                            category_id=category_id)
+        session.add(newItem)
+        session.commit()
+
+        return redirect(url_for('showList', category_id=category_id))
+    else:
+        return render_template('newitem.html', category_id=category_id)
+
 
 app = Flask(__name__)
 
