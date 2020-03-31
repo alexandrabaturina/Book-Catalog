a28b66e508c8c14d4ee9bf7147eec9e21da471a8
alexandrabaturina <alexandra.v.baturina@gmail.com>
2020-03-31 10:27:09 -0700

    Add user table

--- database_setup.py
+++ database_setup.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -5,12 +6,28 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
+class User(Base):
+     __tablename__ = 'user'
+     id = Column(Integer, primary_key = True)
+     name = Column(String(250), nullable = False)
+     email = Column(String(250), nullable = False)
+     picture = Column(String(250))
 
 class Category(Base):
     __tablename__ = 'category'
 
     id = Column(Integer, primary_key = True)
     name = Column(String(50), nullable = False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+# Serialize function for sending JSON objects in a serializable format
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
 
 
 class CatalogItem(Base):
@@ -21,7 +38,8 @@ class CatalogItem(Base):
     description = Column(String(250))
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
-
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 # Serialize function for sending JSON objects in a serializable format
     @property
